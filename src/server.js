7a7d8b9796d1e1f953c0640a2912bbd5e535821f
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-05-10 23:41:24 +0100

    x

--- src/server.js
+++ src/server.js
@@ -163,13 +163,11 @@ io.on("connection",(socket)=>{
   })
 })
 ///////////////////////////////////////////
- 
-if (process.env.NODE_ENV === 'production') {
+  
   // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-} 
+  })
 app.listen(process.env.PORT || 5000);
