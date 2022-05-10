768ed6693aaa44cff3d7bc0827d6bc083341aeb5
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-05-10 22:33:18 +0100

    d

--- backend/src/utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: 'espritla7winta',
-  api_key: '193842965837965',
-  api_secret: 'cMW8cZvEiZUjrqN8Cn_TfzEvm_4',
-});
-
-module.exports = cloudinary;
--- src/utils/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const api = axios.create({
-    BaseURL: "http://127.0.0.1:5000/",
-})
-
-export  default  api;
